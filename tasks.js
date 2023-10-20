e800f94a8aa1d1fc913b3ecf5d6bbd170f0025b3
GenMagadan <genchik111@yandex.ru>
2023-10-20 18:29:33 +0300

    Add task_4

--- tasks.js
+++ tasks.js
@@ -30,19 +30,65 @@
 // Задание 3
 // Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел
 
+// let number1 = parseInt(prompt('Введите первое число'));
+// let number2 = parseInt(prompt('Введите второе число'));
+// let number3 = parseInt(prompt('Введите третье число'));
+
+// let maxNumber = function (a, b, c) {
+//    let max = a;
+//    if (max < b) {
+//       max = b;
+//    }
+//    if (max < c) {
+//       max = c
+//    }
+//    return max;
+// }
+// alert(`Максимальное значение = ${maxNumber(number1, number2, number3)}`);
+
+// Задание 4
+// Необходимо реализовать четыре функции, каждая функция должна принимать по два числа и выполнять одну из операций (каждая функция выполняет одну из них):
+// 1. Сложение
+// 2. Разность
+// 3. Умножение
+// 4. Деление
+
 let number1 = parseInt(prompt('Введите первое число'));
 let number2 = parseInt(prompt('Введите второе число'));
-let number3 = parseInt(prompt('Введите третье число'));
-
-let maxNumber = function (a, b, c) {
-   let max = a;
-   if (max < b) {
-      max = b;
-   }
-   if (max < c) {
-      max = c
-   }
-   return max;
+let add = function (a, b) {
+   return a + b;
 }
-alert(`Максимальное значение = ${maxNumber(number1, number2, number3)}`);
+// или
+// let add = (a, b) => a + b;
+
+let diff = function (a, b) {
+   if (a === b) {
+      return 0;
+   } else if (b > a) {
+      return b - a;
+   } else {
+      return a - b;
+   }
+}
+// или
+// let diff = (a, b) => {
+//    return (a === b) ? 0 : ((a > b) ? a - b : b - a);
+// }
+
+let multi = function (a, b) {
+   return a * b;
+}
+// или
+// let add = (a, b) => a * b;
+
+let div = function (a, b) {
+   return a / b;
+}
+// или
+// let add = (a, b) => a / b;
+
+alert(`Сумма чисел равна ${add(number1, number2)}
+Разность чисел равна ${diff(number1, number2)}
+Произведение чисел равно ${multi(number1, number2)}
+Частное чисел равно ${div(number1, number2)}`);
 
